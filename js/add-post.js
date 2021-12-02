377d5533c63f786d7af4de7b09ac13491371a558
Miro <mirota@metropolia.fi>
2021-12-02 19:48:05 +0200

    Small changes to html structure, preliminary styling for add-post.html + navbar js

--- js/add-post.js
+++ js/add-post.js
@@ -22,3 +22,48 @@ addForm.addEventListener('submit', async (evt) => {
   alert(json.message);
   location.href = 'main-page.html';
 });
+
+/* ----- */
+
+const login = document.getElementById('login');
+const signup = document.getElementById('signup');
+login.style.display = 'none';
+signup.style.display = 'none';
+
+const priceSelect = document.querySelector('#price-select');
+priceSelect.addEventListener("change", () => {
+  if(priceSelect.selectedIndex === 0) {
+    hidePrice();
+  } else
+    showPrice();
+})
+
+function showPrice() {
+  const priceInput = document.querySelector("#price-input");
+  priceInput.classList.add('showPriceInput');
+}
+function hidePrice() {
+  const priceInput = document.querySelector("#price-input");
+  priceInput.classList.remove('showPriceInput');
+}
+
+/* ----- */
+
+
+// NAVIGATION MENU
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((n) =>
+    n.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navMenu.classList.remove('active');
+    })
+);

73c415b6732856779eadd9d72e5cdcdd8f6f444c
Miro <mirota@metropolia.fi>
2021-12-14 23:12:00 +0200

    utils etc

--- js/profile.js
+++ js/profile.js
@@ -15,3 +15,34 @@ function openSignupForm() {
 function closeSignupForm() {
     document.body.classList.remove('showSignupForm');
 }
+
+// SELECT EXISTING MODIFY USER FORM
+const modUserForm = document.querySelector('#modUserForm');
+// Submit modify user form
+modUserForm.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const response = await updateUserProfile(modUserForm);
+    const json = await response.json();
+    if (json.error) {
+        alert(json.error.message);
+    } else {
+        alert(json.message);
+    }
+    location.href = 'main-page.html';
+});
+
+const parsingProfile = async () => {
+    try {
+        const response = await getUserProfile();
+        if (response.ok) {
+            const profile = await response.json();
+            const inputs = modUserForm.querySelectorAll('input');
+            inputs[0].value = profile.username;
+            inputs[1].value = profile.email;
+            console.log('user', user);
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+parsingProfile();

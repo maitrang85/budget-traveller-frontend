5611ba54b78aba188958baa1dd79c2222a9199b9
Miro <mirota@metropolia.fi>
2021-12-16 13:21:29 +0200

    localhost

--- js/add-post.js
+++ js/add-post.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const url = 'http://localhost:3000';
+
 const addForm = document.querySelector('#addPostForm');
 console.log('addForm', addPostForm);
 /* Submit and post camping site form */
